fb54f1d312548e4b132ea11d6b51c4512892d459
Jaemin Cho <heythisischo@gmail.com>
2017-08-11 21:42:42 +0900

    Open broswer: False

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,6 +1,9 @@
 # Configuration file for ipython.
 c = get_config()
 
+# Don't open browser when launching jupyter
+c.NotebookApp.open_browser = False
+
 # Automatic autoimport
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['%autoreload 2']
