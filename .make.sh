c6804d688ed927168f1546217a6f97a1c4b365f4
Jaemin Cho <jmcho@monet.vision>
2017-08-12 02:31:45 +0900

    Add anaconda config

--- .make.sh
+++ .make.sh
@@ -1,5 +1,6 @@
 
 # Vim
+
 # Monokai color scheme
 curl -fLo ~/.vim/colors/monokai.vim --create-dirs \
     https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim
@@ -7,6 +8,7 @@ curl -fLo ~/.vim/colors/monokai.vim --create-dirs \
 # vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
 
 # antigen
 curl -fLo ~/.zsh/antigen.zsh --create-dirs \
@@ -14,3 +16,13 @@ curl -fLo ~/.zsh/antigen.zsh --create-dirs \
 
 # tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# anaconda 3
+if [[ $(uname -s) == Darwin ]]; then
+    curl -fLo /tmp/anaconda3.sh https://repo.continuum.io/archive/Anaconda3-4.4.0-MacOSX-x86_64.sh
+elif [ $(uname -s) == Linux ]; then
+    curl -fLo /tmp/anaconda3.sh https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
+fi
+bash /tmp/anaconda3.sh
+
+conda config --add channels conda-forge
