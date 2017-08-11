da9ac5081e675a6044cfefc52387e603a4522843
Jaemin Cho <jmcho@monet.vision>
2017-08-12 02:55:53 +0900

    base notebook configuration

---
+++ .jupyter/_jupyter_notebook_config.py
@@ -0,0 +1,16 @@
+# Configuration file for jupyter-notebook.
+
+c = get_config()
+
+# Do not open web broswer
+c.NotebookApp.open_browser = False
+
+# Open to all IP
+c.NotebookApp.ip = '*'
+
+# c.NotebookApp.port = 8888
+
+# ipython
+# from notebook.auth import passwd
+# passwd()
+c.NotebookApp.password = u'sha1:....'
