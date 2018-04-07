68ea545d0dd96034c7923f764a2296371f47fd6b
Jaemin Cho <heythisischo@gmail.com>
2018-04-07 11:51:10 +0900

    Update Cuda Path

--- .path.sh
+++ .path.sh
@@ -13,9 +13,10 @@ export TMUXINATOR_CONFIG=$HOME/.tmux
 ln -sf $HOME/.tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/.tmux/tmux-osx.conf $HOME/.tmux-osx.conf
 
-# CUDA 8.0
-export PATH=/usr/local/cuda-8.0/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
+# CUDA
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
 # Virtualenv & Virtualenvwrapper
 # export WORKON_HOME=~/.virtualenvs
 # source /usr/local/bin/virtualenvwrapper.sh
