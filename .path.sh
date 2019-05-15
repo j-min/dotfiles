8651828d82bbd84c3b568605bd1faac402af1712
j-min <heythisischo@gmail.com>
2019-05-15 19:38:15 +0900

    Add Ruby/npm path

--- .path.sh
+++ .path.sh
@@ -8,14 +8,28 @@ if [[ -d "$HOME/anaconda3/bin" ]]; then
 	export PATH=$HOME/anaconda3/bin:$PATH
 fi
 
+# Ruby
+export PATH=$HOME/.rbenv/bin:$PATH
+export PATH=$HOME/.rbenv/plugins/ruby-build/bin:$PATH
+eval "$(rbenv init -)"
+PATH=$HOME/.gem/ruby/2.6.0/bin:$PATH
+
+# npm
+# npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+
 # tmux
 export TMUXINATOR_CONFIG=$HOME/.tmux
 ln -sf $HOME/.tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/.tmux/tmux-osx.conf $HOME/.tmux-osx.conf
 
-# CUDA 8.0
-export PATH=/usr/local/cuda-8.0/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
-# Virtualenv & Virtualenvwrapper
-# export WORKON_HOME=~/.virtualenvs
-# source /usr/local/bin/virtualenvwrapper.sh
+# CUDA 10.0
+export PATH=/usr/local/cuda-10.0/bin:$PATH
+export CUDA_HOME=/usr/local/cuda-10.0
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
+
+# The next line updates PATH for the Google Cloud SDK.
+#if [ -f '/home/j-min/google-cloud-sdk/path.zsh.inc' ]; then source '/home/j-min/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+#if [ -f '/home/j-min/google-cloud-sdk/completion.zsh.inc' ]; then source '/home/j-min/google-cloud-sdk/completion.zsh.inc'; fi
